b45dadd021febef12bca6e421cd891d5d166ba28
danxguerrero <danxguerrero@yahoo.com>
2024-03-03 15:27:39 -0500

    Initalize React app in frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,4 +4,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', async (req, res) => {
+    res.send({
+        "message": "It works"
+    })
+})
+
 module.exports = app;
