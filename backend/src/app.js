15550cd0cd75d33017c7430967762d2b4ef5f6b1
danxguerrero <danxguerrero@yahoo.com>
2024-03-03 22:54:04 -0500

    Implemented a proxy for the frontend to fetch from the backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,10 +4,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', async (req, res) => {
-    res.send({
-        "message": "It works"
-    })
+app.get('/api/items', async (req, res) => {
+    res.send([{
+        id: 1,
+        title: "Laundry",
+        completed: true
+    }, {
+        id: 2,
+        title: "Wash Car",
+        completed: false
+    }])
 })
 
 module.exports = app;
