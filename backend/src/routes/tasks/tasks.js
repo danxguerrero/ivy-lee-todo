52593061407b272ce1203fd6ec2cc27b7a31977f
danxguerrero <danxguerrero@yahoo.com>
2024-03-06 22:46:52 -0500

    Update the create task route to associate to user if user exists.

--- backend/src/routes/tasks/tasks.js
+++ backend/src/routes/tasks/tasks.js
@@ -21,11 +21,28 @@ router.get('/', async (req, res, next) => {
     }
 });
 
-router.put('/', async (req, res, next) => {
-    const {id} = req.user;
+router.post('/', async (req, res, next) => {
+    const { userId, username } = req.user;
     const { task } = req.body;
+
     try{
+        // Find the user in the db
+        const creator = await User.findOne({
+            where: {
+                userId: userId,
+                username: username
+            }
+        })
+
+        if (!creator) {
+            res.sendStatus(404)
+        }
+
+        // Create new Task and associate the user
         const newTask = await Task.create(task)
+        await newTask.setUser(creator)
+
+        res.status(201).send({task: task})
     } catch(error) {
         next(error)
     }
