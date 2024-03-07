4c87f4301c3a806d8512b5eff7c2bca712d089f6
danxguerrero <danxguerrero@yahoo.com>
2024-03-06 22:57:21 -0500

    Added routes to delete and update tasks.

--- backend/src/routes/tasks/tasks.js
+++ backend/src/routes/tasks/tasks.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res, next) => {
         if (!tasks) {
             return res.sendStatus(404)
         }
-        res.send({tasks: tasks})
+        res.send({ tasks: tasks })
     } catch (error) {
         next(error)
     }
@@ -25,25 +25,57 @@ router.post('/', async (req, res, next) => {
     const { userId, username } = req.user;
     const { task } = req.body;
 
-    try{
+    try {
         // Find the user in the db
         const creator = await User.findOne({
             where: {
                 userId: userId,
                 username: username
             }
-        })
+        });
 
         if (!creator) {
-            res.sendStatus(404)
-        }
+            res.sendStatus(404);
+        };
 
         // Create new Task and associate the user
-        const newTask = await Task.create(task)
-        await newTask.setUser(creator)
+        const newTask = await Task.create(task);
+        await newTask.setUser(creator);
 
-        res.status(201).send({task: task})
-    } catch(error) {
+        res.status(201).send({ task: task })
+    } catch (error) {
+        next(error);
+    }
+})
+
+router.delete('/:taskId', async (req, res, next) => {
+    const { taskId } = req.params;
+    try {
+        const taskToDelete = await Task.findByPk(taskId)
+        if (!taskToDelete) {
+            return res.sendStatus(404)
+        };
+
+        await taskToDelete.destroy();
+        res.sendStatus(200);
+    } catch (error) {
+        next(error);
+    }
+})
+
+router.put(':/taskId', async (req, res, next) => {
+    const { taskId } = req.params
+
+    try { 
+        const taskToUpdate = await Task.findByPk(taskId)
+        if (!taskToUpdate) {
+            return res.sendStatus(404)
+        }
+
+        const updatedTask = await Task.update(req.body)
+        res.json(updatedTask)
+        
+    } catch (error) {
         next(error)
     }
 })
