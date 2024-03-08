dc75d0f837e150ff4e040c90f3ee548b713c7eeb
danxguerrero <danxguerrero@yahoo.com>
2024-03-07 23:19:12 -0500

    Began implementing React Bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,30 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
 
 const App = () => {
-  const [areItemsVisible, setAreItemsVisible] = useState(true);
-  const [items, setItems] = useState([]);
+  const [areTasksVisible, setAreTasksVisible] = useState(true);
+  const [tasks, setTasks] = useState([]);
 
-  const getItems = async () => {
+  const getTasks = async () => {
     const response = await fetch('/api/items');
-    const itemsData = await response.json();
-    setItems(itemsData);
+    const tasksData = await response.json();
+    setTasks(tasksData);
   }
 
   useEffect(() => {
-    getItems()
+    getTasks()
   }, [])
 
   return (
     <>
-      <h1>Todos</h1>
-
-      {
-        items.map(item => (
-          <div key={item.id}>
-            <h1>{item.title}</h1>
-            <p>{item.completed.toString()}</p>
-          </div>
-        ))
-      }
-      <button><a href="http://localhost:3000/login">login</a></button>
-      <button><a href="http://localhost:3000/logout">logout</a></button>
+      <main>
+        <Navbar>Todo</Navbar>
+      </main>
     </>
   )
 }
